8a697279cfaa36543d95814a0b59872f6137acb9
Luana Herllain Lopes <elvis.lopes@al.infnet.edu.br>
2022-02-26 13:20:55 -0300

    Habilitando FeignClients e implementando metodo run

--- src/main/java/br/edu/infnet/consumindoapi/ConsumindoApiApplication.java
+++ src/main/java/br/edu/infnet/consumindoapi/ConsumindoApiApplication.java
@@ -1,13 +1,29 @@
 package br.edu.infnet.consumindoapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import java.util.Scanner;
+
+@EnableFeignClients
 @SpringBootApplication
-public class ConsumindoApiApplication {
+public class ConsumindoApiApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(ConsumindoApiApplication.class, args);
     }
 
+    @Autowired
+    private IClienteAPI clienteAPI;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Entre com o filme a consultar: ");
+        String filme = entrada.nextLine();
+        System.out.println(clienteAPI.buscarFilmePor(filme));
+    }
 }
